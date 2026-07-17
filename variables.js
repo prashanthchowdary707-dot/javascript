d98956d4d477ae968ec7248583702037ad30b7ed
prashanthchowdary707-dot <prashanthchowdary707@gmail.com>
2026-07-17 08:23:38 +0000

    variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountid = 12234
+let accountNumber = "32232"
+var accountState = "bengaluru"
+
+//accountid = 2345;//const variable is unchangable
+
+accountNumber = 78787;//let is changable
+
+/*
+prefer not to use var becz of the issue in block space and functional space
+*/
+
+console.log(accountNumber);
