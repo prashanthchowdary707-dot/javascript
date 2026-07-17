444bd1578b30b72d23ee1e83192871319f3832d7
prashanthchowdary707-dot <prashanthchowdary707@gmail.com>
2026-07-17 10:27:28 +0000

    operations

---
+++ operations.js
@@ -0,0 +1,33 @@
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);   
+console.log(2%3);*/
+
+let str1 = "prashanth"
+let str2 = "reddy"
+let str3 = str1 + " " + str2
+console.log(str3);
+
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2 + 2); //122
+console.log(1+2+"2"); //32
+
+console.log(+true) //1
+//console.log(false+)//error
+
+let gamechanger=100
+//gamechanger++
+//console.log(gamechanger)
+
+++gamechanger
+console.log(gamechanger)
+
+
+
