e714251f36955ea0e5bd15d74e7d13b268e8fa9d
prashanthchowdary707-dot <prashanthchowdary707@gmail.com>
2026-07-17 09:37:34 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all the js code in the newer version
+
+console.log(3
+    +3); //the code should be more readable
+
+let name = "prashanth"
+let age = 22
+let isloggedin =  true
+
+console.log(isloggedin);
+
+// number => 2 to power of 53
+//string => ""
+// boolean => true/false
+// null => standalone value and it is a type and it is a object
+// undefined => value didnt assigned
+//symbol => we use this for uniqueness
+// object =>
+
+
+console.log(typeof null);
+console.log(typeof undefined);
+
